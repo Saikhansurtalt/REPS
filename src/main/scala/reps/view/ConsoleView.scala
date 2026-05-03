12052ea2c63379614d3e5365f6d59d97489b9a8c
saikhansurtalt <saikhansurtalt.sumiyabazar@student.lut.fi>
2026-05-03 13:30:28 +0300

    Added names of members

--- src/main/scala/reps/view/ConsoleView.scala
+++ src/main/scala/reps/view/ConsoleView.scala
@@ -1,3 +1,16 @@
+/*
+ * Project: REPS – Renewable Energy Production System
+ *
+ * Course: Functional Programming
+ *
+ * Team Members:
+ * Saikhansurtalt Sumiyabazar 002329527
+ * Inthuja Inthumathan 002599632
+ *
+ * Description:
+ * This file is part of the REPS project submission.
+ */
+
 package reps.view
 
 import reps.model.*
